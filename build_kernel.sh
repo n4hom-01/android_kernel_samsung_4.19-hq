dfeda9a887d2a8b97608de70b8dfc28dc27e7dd9
Nahom <168488759+n4hom-01@users.noreply.github.com>
2025-12-02 07:18:33 +0000

    add toolchains and configs

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
+export RDIR="$(pwd)"
+export KBUILD_BUILD_USER="n4hom"
+export KBUILD_BUILD_HOST="github"
 
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+#init sukisu-ultra
+git submodule init && git submodule update
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a04e_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+#export toolchain paths
+export BUILD_CROSS_COMPILE=${RDIR}/toolchain/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+export BUILD_CC=${RDIR}/toolchain/clang-r383902/bin/clang
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+#output dir
+if [ ! -d "${RDIR}/out" ]; then
+    mkdir -p "${RDIR}/out"
+fi
+
+#build dir
+if [ ! -d "${RDIR}/build" ]; then
+    mkdir -p "${RDIR}/build"
+else
+    rm -rf "${RDIR}/build" && mkdir -p "${RDIR}/build"
+fi
+
+#build options
+export ARGS="
+-C $(pwd) \
+O=$(pwd)/out \
+-j$(nproc) \
+ARCH=arm64 \
+CROSS_COMPILE=${BUILD_CROSS_COMPILE} \
+CC=${BUILD_CC} \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+KCFLAGS=-w \
+CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
+"
+
+#build kernel image
+build_kernel(){
+    make ${ARGS} clean && make ${ARGS} mrproper
+    make ${ARGS} a04e_defconfig custom.config chroot.config
+    make ${ARGS} || exit 1
+    cp out/arch/arm64/boot/Image.gz $(pwd)/arch/arm64/boot/Image.gz
+}
+
+build_kernel
